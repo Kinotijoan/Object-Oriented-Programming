753c449ce67817c9f4b74e71646208ff5065a3f0
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-10-06 11:56:08 +0300

    OOP

---
+++ JAVA/Class.java
@@ -0,0 +1,42 @@
+public class loops{
+    public static void main(String[] args){
+        System.out.println("Good Afternoon");
+        // for(int k =0; k < 10; k++){
+        //     if(k==5) {
+        //     continue;
+        //     }
+        //     System.out.println("it is okay");
+        // }
+        // int x = 0;
+        // while(x < 10){
+        //     System.out.println("num1");
+        // }
+
+        int[] myArray = {30,70,20,60,60,15};
+        // System.out.println(myArray[0]);
+        // for(int i = 0; i < myArray.length; i++){
+        //     System.out.println(myArray[i]);
+        // }
+        int k = 0;
+        for(int i = 0 ; i < myArray.length; i++){
+            if(myArray[i] == 40){
+                continue;
+            }
+            k += myArray[i];
+        //     if(k > 50){
+        //         break;
+        //     }
+        // }
+        System.out.println(k);
+         
+        // int[] myArray2 = new int [10];
+        // myArray2[0] = 20;
+        // System.out.println(myArray2[0]);
+
+        int multiArray[][] = new int [10][10];
+
+
+
+
+    }
+}
